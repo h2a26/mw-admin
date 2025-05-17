18a59ed0167aeb234182b6ac0b66946e4273f32b
heinhtetaung <hha.heinms@gmail.com>
2025-05-17 12:12:42 +0630

    fix: remove user registration

--- src/main/java/org/hein/api/controller/AuthApi.java
+++ src/main/java/org/hein/api/controller/AuthApi.java
@@ -2,11 +2,8 @@ package org.hein.api.controller;
 
 import org.hein.api.input.auth.TokenRefreshForm;
 import org.hein.api.input.auth.TokenRequestForm;
-import org.hein.api.input.auth.UserRegistrationRequest;
 import org.hein.api.output.auth.TokenResponse;
-import org.hein.api.output.auth.UserRegistrationResponse;
 import org.hein.security.token.TokenManagementService;
-import org.hein.service.AuthService;
 import org.hein.utils.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,18 +14,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthApi {
 
-    private final AuthService authService;
     private final TokenManagementService tokenService;
 
-    public AuthApi(AuthService authService, TokenManagementService tokenService) {
-        this.authService = authService;
+    public AuthApi(TokenManagementService tokenService) {
         this.tokenService = tokenService;
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<ApiResponse<UserRegistrationResponse>> register(@Validated @RequestBody UserRegistrationRequest userRegistrationRequest, BindingResult result) {
-        UserRegistrationResponse userRegistrationResponse = authService.registerUser(userRegistrationRequest);
-        return ApiResponse.of(userRegistrationResponse);
     }
 
     @PostMapping("/login")
